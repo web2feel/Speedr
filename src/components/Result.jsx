5da9c6bf004421925ae0b454b9313796d3fe6978
Jinson <web2feel@gmail.com>
2023-01-31 15:35:37 +0530

    updates

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,8 +3,11 @@ import React from "react";
 function Result({ result }) {
   return <div className="bg-gradient-to-br from-indigo-700 to-indigo-900  shadow rounded-lg p-8 my-4 text-center text-2xl text-white">
       Your media will run for <br/>
-      <span className="text-4xl"> {result.hours} {result.hours > 1 ? "Hours" : "Hour"} and {result.minutes} Minutes </span>
-  </div>;
+      <span className="text-4xl">
+      { result.hours > 1 ? `${result.hours} Hours ` : `${result.hours} Hour `}
+      { result.minutes > 0 ? `${result.minutes} Minutes` : ''}
+      </span>
+   </div>;
 }
 
 export default Result;
